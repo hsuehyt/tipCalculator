af5df64da6d98e946b7e827fcc89836447c3ee3a
hsuehyt <hsueh.yuting@gmail.com>
2024-04-12 00:49:03 +0800

    create

---
+++ tipCalculator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class tipCalculator : MonoBehaviour
+{
+    private TextMeshProUGUI textMeshPro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CalculateTip()
+    {
+    }
+}
